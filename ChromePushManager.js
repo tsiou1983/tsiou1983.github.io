174367b1f951e9ba238203ca53bc7fd9c5250511
tsiou1983 <gkopanas@hotmail.com>
2016-01-18 15:29:07 +0200

    Update ChromePushManager.js

--- ChromePushManager.js
+++ ChromePushManager.js
@@ -28,7 +28,7 @@ ChromePushManager.subscribeBrowserId = function(callback) {
       .then(function(subscription) {  
         var register = ChromePushManager.getRegistrationId(subscription);
         
-        console.log(register);
+        console.log(subscription);
         callback(null, register);
       })  
       .catch(function(e) {  
